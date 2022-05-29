bbee808493c305ef01ca15ea99fce848a8462145
Vlodumyr <96140667+Vlodumyr@users.noreply.github.com>
2022-05-29 13:57:54 +0300

    Delete CQRS directory

--- CQRS/UserAccounts/Views/UserAccountRoleView.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace InventoryСontrol.Application.CQRS.UserAccounts.Views
-{
-    public class UserAccountRoleView : UserAccountView
-    {
-        public List<string> Roles { get; set; }
-    }
-}
